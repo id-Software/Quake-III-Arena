18fc0fb5f523710d376a9bdf48a10428e80ce1a9
Ludwig Nussel <ludwig.nussel@suse.de>
2005-09-17 14:20:22 +0000

    fix crash when starting a TA CTF game with shared libs (#2367)

--- code/game/g_team.c
+++ code/game/g_team.c
@@ -47,8 +47,9 @@ void Team_InitGame( void ) {
 
 	switch( g_gametype.integer ) {
 	case GT_CTF:
-		teamgame.redStatus = teamgame.blueStatus = -1; // Invalid to force update
+		teamgame.redStatus = -1; // Invalid to force update
 		Team_SetFlagStatus( TEAM_RED, FLAG_ATBASE );
+		 teamgame.blueStatus = -1; // Invalid to force update
 		Team_SetFlagStatus( TEAM_BLUE, FLAG_ATBASE );
 		break;
 #ifdef MISSIONPACK
