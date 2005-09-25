e66a28dfeb4bb6ccc92a47702083ecf646f1b25c
Tim Angus <tim@ngus.net>
2005-09-25 21:43:48 +0000

    * Removed cons based build system * Removed some id specific shell/perl scripts * Removed some unused asm files * Removed prehistoric RPM build rules

--- code/unix/extract_ver.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-# extracting version string from game/q_shared.h
-# hacked from Wolf build process
-
-# extract the wolf version from q_shared.h
-$line = `cat ../game/q_shared.h | grep Q3_VERSION`;
-chomp $line;
-$line =~ s/.*Q3\ (.*)\"/$1/;
-print "$line\n";
