acb810b9723ff79f7ab42af865f6d36c115bd542
Tim Angus <tim@ngus.net>
2005-12-01 20:45:01 +0000

    * Fix compilation when using SDL and dlopening OpenAL

--- code/client/qal.c
+++ code/client/qal.c
@@ -45,8 +45,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define OBJFREE(x) FreeLibrary(x)
 
 #elif defined __linux__ || defined __FreeBSD__ || defined MACOS_X
-#include <unistd.h>
-#include <sys/types.h>
 #include <dlfcn.h>
 #define OBJTYPE void *
 #define OBJLOAD(x) dlopen(x, RTLD_LAZY | RTLD_GLOBAL)
@@ -55,6 +53,11 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #else
 
 #error "Your platform has no lib loading code or it is disabled"
+#endif
+
+#if defined __linux__ || defined __FreeBSD__ || defined MACOS_X
+#include <unistd.h>
+#include <sys/types.h>
 #endif
 
 LPALENABLE qalEnable;
