0f8c177cf392d1e536682d0c9f5a1ab5ff232561
Tim Angus <tim@ngus.net>
2005-12-02 00:14:24 +0000

    * Fix to an OpenAL "bug"

--- code/client/snd_openal.c
+++ code/client/snd_openal.c
@@ -653,12 +653,17 @@ srcHandle_t S_AL_SrcAlloc( alSrcPriority_t priority, int entnum, int channel )
 			}
 		}
 
+		// The channel system is not actually adhered to by baseq3, and not
+		// implemented in snd_dma.c, so while the following is strictly correct, it
+		// causes incorrect behaviour versus defacto baseq3
+#if 0
 		// Is it an exact match, and not on channel 0?
 		if((srcList[i].entity == entnum) && (srcList[i].channel == channel) && (channel != 0))
 		{
 			S_AL_SrcKill(i);
 			return i;
 		}
+#endif
 	}
 
 	// Do we have an empty one?
