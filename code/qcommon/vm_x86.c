e95f1f210629574e819019ddb38ba0e6906291da
Tim Angus <tim@ngus.net>
2006-01-06 00:04:05 +0000

    * Fix to the gcc4/-O0 x86 JIT compiler bug

--- code/qcommon/vm_x86.c
+++ code/qcommon/vm_x86.c
@@ -229,7 +229,7 @@ void AsmCall( void ) {
 		"doret:						\n\t" \
 		"	ret					\n\t" \
 		: "=rm" (callSyscallNum), "=rm" (callProgramStack), "=rm" (callOpStack) \
-		: "rm" (instructionPointers) \
+		: "m" (instructionPointers) \
 		: "ax", "di", "si", "cx" \
 	);
 }
