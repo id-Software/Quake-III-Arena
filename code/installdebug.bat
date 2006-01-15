ede5c811c2e326ea86ca16989daf38a646455020
Tim Angus <tim@ngus.net>
2006-01-15 21:55:43 +0000

    * Deleted the .lnt and .bat files in the code dir

--- code/installdebug.bat
+++
@@ -1,6 +0,0 @@
-call closefiles
-copy debug\quake3.exe g:\quake3
-copy debug\qagamex86.dll g:\quake3
-copy debug\cgamex86.dll g:\quake3
-copy debug\uix86.dll g:\quake3
-
--- code/installvms.bat
+++
@@ -1,7 +0,0 @@
-copy \quake3\missionpack\vm\cgame.* g:\quake3\missionpack\vm
-copy \quake3\missionpack\vm\qagame.* g:\quake3\missionpack\vm
-copy \quake3\missionpack\vm\ui.* g:\quake3\missionpack\vm
-copy \quake3\baseq3\vm\cgame.* g:\quake3\baseq3\vm
-copy \quake3\baseq3\vm\qagame.* g:\quake3\baseq3\vm
-copy \quake3\baseq3\vm\q3_ui.qvm g:\quake3\baseq3\vm\ui.qvm
-
