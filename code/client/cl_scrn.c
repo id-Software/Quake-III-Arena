659ded3a41effe202eb1a2943c1d01698e5e2bbe
Tim Angus <tim@ngus.net>
2006-02-21 21:29:26 +0000

    * Fix to bug #2599 (fullscreen cinematics)

--- code/client/cl_scrn.c
+++ code/client/cl_scrn.c
@@ -440,7 +440,7 @@ void SCR_DrawScreenField( stereoFrame_t stereoFrame ) {
 
 	// wide aspect ratio screens need to have the sides cleared
 	// unless they are displaying game renderings
-	if ( cls.state != CA_ACTIVE ) {
+	if ( cls.state != CA_ACTIVE && cls.state != CA_CINEMATIC ) {
 		if ( cls.glconfig.vidWidth * 480 > cls.glconfig.vidHeight * 640 ) {
 			re.SetColor( g_color_table[0] );
 			re.DrawStretchPic( 0, 0, cls.glconfig.vidWidth, cls.glconfig.vidHeight, 0, 0, 0, 0, cls.whiteShader );
