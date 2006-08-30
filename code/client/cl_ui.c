bc4591b03e666dceaa669ab4f9fc85b45e8c5da6
Tony J. White = <tjw@tjw.org>
2006-08-30 15:16:36 +0000

    * renamed static function GetClipboardData to CL_GetClipboardData since it was causing compile errors in newer mingw

--- code/client/cl_ui.c
+++ code/client/cl_ui.c
@@ -618,10 +618,10 @@ static void CL_GetGlconfig( glconfig_t *config ) {
 
 /*
 ====================
-GetClipboardData
+CL_GetClipboardData
 ====================
 */
-static void GetClipboardData( char *buf, int buflen ) {
+static void CL_GetClipboardData( char *buf, int buflen ) {
 	char	*cbd;
 
 	cbd = Sys_GetClipboardData();
@@ -934,7 +934,7 @@ intptr_t CL_UISystemCalls( intptr_t *args ) {
 		return 0;
 
 	case UI_GETCLIPBOARDDATA:
-		GetClipboardData( VMA(1), args[2] );
+		CL_GetClipboardData( VMA(1), args[2] );
 		return 0;
 
 	case UI_GETCLIENTSTATE:
