ecbb43c9b09e089e2b2c4a55ed763e71ef779424
Zachary Slater <zachary@ioquake.org>
2007-06-13 20:41:06 +0000

    gcc 4.2 patch from nyhm of gentoo (thanks!)

--- code/tools/asm/cmdlib.c
+++ code/tools/asm/cmdlib.c
@@ -312,7 +312,7 @@ char *ExpandArg (const char *path)
 char *ExpandPath (const char *path)
 {
 	static char full[1024];
-	if (!qdir)
+	if (!qdir[0])
 		Error ("ExpandPath called without qdir set");
 	if (path[0] == '/' || path[0] == '\\' || path[1] == ':') {
 		strcpy( full, path );
@@ -325,7 +325,7 @@ char *ExpandPath (const char *path)
 char *ExpandGamePath (const char *path)
 {
 	static char full[1024];
-	if (!qdir)
+	if (!qdir[0])
 		Error ("ExpandGamePath called without qdir set");
 	if (path[0] == '/' || path[0] == '\\' || path[1] == ':') {
 		strcpy( full, path );
