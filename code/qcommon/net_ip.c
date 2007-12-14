662b67c733a3e4e5d6c7fda0bb88ae2732b22209
Thilo Schulz <arny@ats.s.bawue.de>
2007-12-14 16:25:33 +0000

    Replace obsolete macro with some other macro. I hope this does not break too much.

--- code/qcommon/net_ip.c
+++ code/qcommon/net_ip.c
@@ -756,7 +756,7 @@ void NET_GetLocalAddress( void ) {
 					if (sdl->sdl_type != IFT_LOOP) {
 						// Get the local interface address
 						strncpy(ifr.ifr_name, inetInterface->ifr_name, sizeof(ifr.ifr_name));
-						if (ioctl(interfaceSocket, OSIOCGIFADDR, (caddr_t)&ifr) < 0) {
+						if (ioctl(interfaceSocket, SIOCGIFADDR, (caddr_t)&ifr) < 0) {
 							Com_Printf("NET_GetLocalAddress: Unable to get local address "
 									"for interface '%s', errno = %d\n", inetInterface->ifr_name, errno);
 						} else {
