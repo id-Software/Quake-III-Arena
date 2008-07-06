6b572920a9b7de9c82634c244250501b2589da89
Tim Angus <tim@ngus.net>
2008-07-06 01:35:01 +0000

    * Remove silly range check on sv_voip during initialisation * Changed dedicated binary dedicated cvar to CVAR_INIT and remove runtime check

--- code/server/sv_init.c
+++ code/server/sv_init.c
@@ -656,10 +656,6 @@ void SV_Init (void) {
 	sv_pure = Cvar_Get ("sv_pure", "1", CVAR_SYSTEMINFO );
 #if USE_VOIP
 	sv_voip = Cvar_Get ("sv_voip", "1", CVAR_SYSTEMINFO | CVAR_LATCH);
-	if ( (sv_voip->integer < 0) || (sv_voip->integer > 1) ) {
-		Com_Printf("WARNING: sv_voip must be 0 or 1. Setting to 1.");
-		Cvar_Set ("sv_voip", "1");
-	}
 #endif
 	Cvar_Get ("sv_paks", "", CVAR_SYSTEMINFO | CVAR_ROM );
 	Cvar_Get ("sv_pakNames", "", CVAR_SYSTEMINFO | CVAR_ROM );
