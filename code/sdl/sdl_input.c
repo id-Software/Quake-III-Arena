42e444ea0b0eae84f0908be937bcf871f3d044da
Tim Angus <tim@ngus.net>
2008-07-16 17:45:10 +0000

    * Revert r1341 as there doesn't seem to be a lot of point to the change and it   potentially will cause input events to be queued in a different order to that   in which they were received

--- code/sdl/sdl_input.c
+++ code/sdl/sdl_input.c
@@ -653,7 +653,6 @@ static void IN_ProcessEvents( void )
 	SDL_Event e;
 	const char *p = NULL;
 	int key = 0;
-	int mx = 0, my = 0;
 
 	if( !SDL_WasInit( SDL_INIT_VIDEO ) )
 			return;
@@ -694,10 +693,7 @@ static void IN_ProcessEvents( void )
 
 			case SDL_MOUSEMOTION:
 				if (mouseActive)
-				{
-					mx += e.motion.xrel;
-					my += e.motion.yrel;
-				}
+					Com_QueueEvent( 0, SE_MOUSE, e.motion.xrel, e.motion.yrel, 0, NULL );
 				break;
 
 			case SDL_MOUSEBUTTONDOWN:
@@ -737,9 +733,6 @@ static void IN_ProcessEvents( void )
 				break;
 		}
 	}
-
-	if(mx || my)
-		Com_QueueEvent( 0, SE_MOUSE, mx, my, 0, NULL );
 }
 
 /*
