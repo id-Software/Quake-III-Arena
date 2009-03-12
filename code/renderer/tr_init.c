18b27806bcb73dad53f76f67aca0bd51ce065a95
Tim Angus <tim@ngus.net>
2009-03-12 19:20:20 +0000

    * Remove unexplainable ( tr.overbrightBits > 0 ) condition when correcting   gamma in screenshots

--- code/renderer/tr_init.c
+++ code/renderer/tr_init.c
@@ -376,7 +376,7 @@ void RB_TakeScreenshot( int x, int y, int width, int height, char *fileName ) {
 	}
 
 	// gamma correct
-	if ( ( tr.overbrightBits > 0 ) && glConfig.deviceSupportsGamma ) {
+	if ( glConfig.deviceSupportsGamma ) {
 		R_GammaCorrect( buffer + 18, glConfig.vidWidth * glConfig.vidHeight * 3 );
 	}
 
@@ -398,7 +398,7 @@ void RB_TakeScreenshotJPEG( int x, int y, int width, int height, char *fileName 
 	qglReadPixels( x, y, width, height, GL_RGBA, GL_UNSIGNED_BYTE, buffer ); 
 
 	// gamma correct
-	if ( ( tr.overbrightBits > 0 ) && glConfig.deviceSupportsGamma ) {
+	if ( glConfig.deviceSupportsGamma ) {
 		R_GammaCorrect( buffer, glConfig.vidWidth * glConfig.vidHeight * 4 );
 	}
 
@@ -553,7 +553,7 @@ void R_LevelShot( void ) {
 	}
 
 	// gamma correct
-	if ( ( tr.overbrightBits > 0 ) && glConfig.deviceSupportsGamma ) {
+	if ( glConfig.deviceSupportsGamma ) {
 		R_GammaCorrect( buffer + 18, 128 * 128 * 3 );
 	}
 
@@ -702,7 +702,7 @@ const void *RB_TakeVideoFrameCmd( const void *data )
 			GL_UNSIGNED_BYTE, cmd->captureBuffer );
 
 	// gamma correct
-	if( ( tr.overbrightBits > 0 ) && glConfig.deviceSupportsGamma )
+	if( glConfig.deviceSupportsGamma )
 		R_GammaCorrect( cmd->captureBuffer, cmd->width * cmd->height * 4 );
 
 	if( cmd->motionJpeg )
