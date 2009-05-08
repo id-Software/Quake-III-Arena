f0e24f9a464064f50bd447bbabb18b8873f0be2b
Ludwig Nussel <ludwig.nussel@suse.de>
2009-05-08 09:48:40 +0000

    msg->oob is actually qboolean (#4010)

--- code/server/sv_net_chan.c
+++ code/server/sv_net_chan.c
@@ -36,7 +36,8 @@ SV_Netchan_Encode
 static void SV_Netchan_Encode( client_t *client, msg_t *msg ) {
 	long reliableAcknowledge, i, index;
 	byte key, *string;
-        int	srdc, sbit, soob;
+        int	srdc, sbit;
+        qboolean soob;
         
 	if ( msg->cursize < SV_ENCODE_START ) {
 		return;
@@ -48,7 +49,7 @@ static void SV_Netchan_Encode( client_t *client, msg_t *msg ) {
         
         msg->bit = 0;
         msg->readcount = 0;
-        msg->oob = 0;
+        msg->oob = qfalse;
         
 	reliableAcknowledge = MSG_ReadLong(msg);
 
@@ -89,14 +90,15 @@ SV_Netchan_Decode
 */
 static void SV_Netchan_Decode( client_t *client, msg_t *msg ) {
 	int serverId, messageAcknowledge, reliableAcknowledge;
-	int i, index, srdc, sbit, soob;
+	int i, index, srdc, sbit;
+	qboolean soob;
 	byte key, *string;
 
         srdc = msg->readcount;
         sbit = msg->bit;
         soob = msg->oob;
         
-        msg->oob = 0;
+        msg->oob = qfalse;
         
         serverId = MSG_ReadLong(msg);
 	messageAcknowledge = MSG_ReadLong(msg);
