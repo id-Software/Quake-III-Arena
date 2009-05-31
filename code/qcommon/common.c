e0a2100df5cd458162b47be26368286cbe604913
Thilo Schulz <arny@ats.s.bawue.de>
2009-05-31 20:31:56 +0000

    Fix warning in Sys_RandomBytes() usage

--- code/qcommon/common.c
+++ code/qcommon/common.c
@@ -2509,7 +2509,7 @@ static void Com_InitRand(void)
 {
 	unsigned int seed;
 
-	if(Sys_RandomBytes(&seed, sizeof(seed)))
+	if(Sys_RandomBytes((byte *) &seed, sizeof(seed)))
 		srand(seed);
 	else
 		srand(time(NULL));
