a7417a4bde87dcdb9c1a66c00b3a3f43de2e296c
Thilo Schulz <arny@ats.s.bawue.de>
2009-06-10 09:00:31 +0000

    minor code cleanup

--- code/renderer/tr_main.c
+++ code/renderer/tr_main.c
@@ -513,22 +513,19 @@ R_SetupProjection
 void R_SetupProjection(viewParms_t *dest, float zProj, qboolean computeFrustum)
 {
 	float	xmin, xmax, ymin, ymax;
-	float	width, height, stereoSep = r_stereoSeparation->value;
+	float	width, height, stereoSep;
 
 	/*
 	 * offset the view origin of the viewer for stereo rendering 
 	 * by setting the projection matrix appropriately.
 	 */
 
-	if(stereoSep != 0)
-	{
-		if(dest->stereoFrame == STEREO_LEFT)
-			stereoSep = zProj / r_stereoSeparation->value;
-		else if(dest->stereoFrame == STEREO_RIGHT)
-			stereoSep = zProj / -r_stereoSeparation->value;
-		else
-			stereoSep = 0;
-	}
+	if(dest->stereoFrame == STEREO_LEFT)
+		stereoSep = zProj / r_stereoSeparation->value;
+	else if(dest->stereoFrame == STEREO_RIGHT)
+		stereoSep = zProj / -r_stereoSeparation->value;
+	else
+		stereoSep = 0;
 
 	ymax = zProj * tan(dest->fovY * M_PI / 360.0f);
 	ymin = -ymax;
