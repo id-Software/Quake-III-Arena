fa3f044257bbcfc3d655bb9677198b523de50291
Zachary Slater <zachary@ioquake.org>
2009-06-14 21:45:55 +0000

    This patch from Monk fixes the q3asm help screen Bugzilla bug 3986 http://bugzilla.icculus.org/show_bug.cgi?id=3986

--- code/tools/asm/q3asm.c
+++ code/tools/asm/q3asm.c
@@ -1520,12 +1520,13 @@ static void ShowHelp( char *argv0 ) {
 	Error("Usage: %s [OPTION]... [FILES]...\n\
 Assemble LCC bytecode assembly to Q3VM bytecode.\n\
 \n\
-    -o OUTPUT      Write assembled output to file OUTPUT.qvm\n\
-    -f LISTFILE    Read options and list of files to assemble from LISTFILE.q3asm\n\
-    -b BUCKETS     Set symbol hash table to BUCKETS buckets\n\
-    -v             Verbose compilation report\n\
-    -vq3           Produce a qvm file compatible with Q3 1.32b\n\
-    -h --help -?   Show this help\n\
+  -o OUTPUT      Write assembled output to file OUTPUT.qvm\n\
+  -f LISTFILE    Read options and list of files to assemble from LISTFILE.q3asm\n\
+  -b BUCKETS     Set symbol hash table to BUCKETS buckets\n\
+  -m             Generate a mapfile for each OUTPUT.qvm\n\
+  -v             Verbose compilation report\n\
+  -vq3           Produce a qvm file compatible with Q3 1.32b\n\
+  -h --help -?   Show this help\n\
 ", argv0);
 }
 
