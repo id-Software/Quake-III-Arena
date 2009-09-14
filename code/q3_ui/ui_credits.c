8bb2f9a5f5293bd6a3143c2eec7a10bff0201324
Ryan C. Gordon <icculus@icculus.org>
2009-09-14 21:08:08 +0000

    Threw in a hack to fix rendering of credits menu in widescreen.
      Fixes Bugzilla #2744.

--- code/q3_ui/ui_credits.c
+++ code/q3_ui/ui_credits.c
@@ -166,6 +166,12 @@ UI_CreditMenu
 ===============
 */
 void UI_CreditMenu( void ) {
+	/* This UI_FillRect() hack will blank the borders if you're in widescreen,
+	   so you get a completely black background instead of stripes from the
+	   previous frame on each side of the credits.. */
+	const float black[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
+	UI_FillRect(0 - uis.bias, 0, (640.0f / uis.xscale) * 2.0f, 480.0f / uis.yscale, black);
+
 	memset( &s_credits, 0 ,sizeof(s_credits) );
 
 	s_credits.menu.draw = UI_CreditMenu_Draw;
