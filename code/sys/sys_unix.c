f89092fda635bc721fbf02c3b5dc069db95888eb
Tim Angus <tim@ngus.net>
2009-09-15 09:48:11 +0000

    * Revert r1624 and r1626-r1628

--- code/sys/sys_unix.c
+++ code/sys/sys_unix.c
@@ -54,9 +54,7 @@ char *Sys_DefaultHomePath(void)
 		if( ( p = getenv( "HOME" ) ) != NULL )
 		{
 			Q_strncpyz( homePath, p, sizeof( homePath ) );
-#ifdef HOMEPATH
-			Q_strcat( homePath, sizeof( homePath ), HOMEPATH );
-#elif defined(MACOS_X)
+#ifdef MACOS_X
 			Q_strcat( homePath, sizeof( homePath ), "/Library" );
 			mkdir( homePath, 0750 );  /* just in case. */
 			Q_strcat( homePath, sizeof( homePath ), "/Application Support" );
