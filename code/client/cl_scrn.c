0bdf52e2fbad72dff27724cff020590de504ab5d
Thilo Schulz <arny@ats.s.bawue.de>
2009-10-25 19:51:32 +0000

    Show color codes in full length, patch by Amanieu (#4318)

--- code/client/cl_scrn.c
+++ code/client/cl_scrn.c
@@ -273,14 +273,16 @@ void SCR_DrawSmallStringExt( int x, int y, const char *string, float *setColor, 
 	xx = x;
 	re.SetColor( setColor );
 	while ( *s ) {
-		if ( !noColorEscape && Q_IsColorString( s ) ) {
+		if ( Q_IsColorString( s ) ) {
 			if ( !forceColor ) {
 				Com_Memcpy( color, g_color_table[ColorIndex(*(s+1))], sizeof( color ) );
 				color[3] = setColor[3];
 				re.SetColor( color );
 			}
-			s += 2;
-			continue;
+			if ( !noColorEscape ) {
+				s += 2;
+				continue;
+			}
 		}
 		SCR_DrawSmallChar( xx, y, *s );
 		xx += SMALLCHAR_WIDTH;
