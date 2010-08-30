fa34239f581d164756a3e4bdda2d57c4197b697f
Thilo Schulz <arny@ats.s.bawue.de>
2010-08-30 23:25:44 +0000

    another fix. Thanks Zakk Middleton

--- code/renderer/tr_model.c
+++ code/renderer/tr_model.c
@@ -562,8 +562,8 @@ static qboolean R_LoadMDR( model_t *mod, void *buffer, int filesize, const char 
 				((float *)frame->bones)[j] = LittleFloat( ((float *)curframe->bones)[j] );
 			}
 			
-			curframe = (mdrFrame_t *) &curframe->bones[j];
-			frame = (mdrFrame_t *) &frame->bones[j];
+			curframe = (mdrFrame_t *) &curframe->bones[mdr->numBones];
+			frame = (mdrFrame_t *) &frame->bones[mdr->numBones];
 		}
 	}
 	
