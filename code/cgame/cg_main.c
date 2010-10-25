956f4957263cac1b38e56510dc5f15673ee11d4b
Zachary Slater <zachary@ioquake.org>
2010-10-25 06:34:33 +0000

    Bugzilla #4749 TAHUD: Fix hud fallback

--- code/cgame/cg_main.c
+++ code/cgame/cg_main.c
@@ -1443,7 +1443,7 @@ void CG_LoadMenus(const char *menuFile) {
 
 	len = trap_FS_FOpenFile( menuFile, &f, FS_READ );
 	if ( !f ) {
-		trap_Error( va( S_COLOR_YELLOW "menu file not found: %s, using default\n", menuFile ) );
+		Com_Printf( S_COLOR_YELLOW "menu file not found: %s, using default\n", menuFile );
 		len = trap_FS_FOpenFile( "ui/hud.txt", &f, FS_READ );
 		if (!f) {
 			trap_Error( va( S_COLOR_RED "default menu file not found: ui/hud.txt, unable to continue!\n") );
