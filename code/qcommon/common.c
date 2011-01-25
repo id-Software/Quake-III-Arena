74be070642ca4e86215d07e82cc0093c12790bad
Thilo Schulz <arny@ats.s.bawue.de>
2011-01-25 16:18:49 +0000

    Make sure com_developer global var gets written right after developer has been read from cmd line

--- code/qcommon/common.c
+++ code/qcommon/common.c
@@ -2665,6 +2665,7 @@ void Com_Init( char *commandLine ) {
 
 	// get the developer cvar set as early as possible
 	Com_StartupVariable( "developer" );
+	com_developer = Cvar_Get("developer", "0", CVAR_TEMP);
 
 	// done early so bind command exists
 	CL_InitKeyCommands();
@@ -2714,7 +2715,6 @@ void Com_Init( char *commandLine ) {
 	com_maxfps = Cvar_Get ("com_maxfps", "85", CVAR_ARCHIVE);
 	com_blood = Cvar_Get ("com_blood", "1", CVAR_ARCHIVE);
 
-	com_developer = Cvar_Get ("developer", "0", CVAR_TEMP );
 	com_logfile = Cvar_Get ("logfile", "0", CVAR_TEMP );
 
 	com_timescale = Cvar_Get ("timescale", "1", CVAR_CHEAT | CVAR_SYSTEMINFO );
