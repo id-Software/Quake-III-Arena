4982bf8e6b0c9f16fc2c2a59c4dda4174a45f7b5
Thilo Schulz <arny@ats.s.bawue.de>
2011-02-02 16:49:52 +0000

    r1833 now made some Linux systems spit out warnings. Fix this once and for all

--- code/qcommon/cvar.c
+++ code/qcommon/cvar.c
@@ -1210,9 +1210,9 @@ void	Cvar_Update( vmCvar_t *vmCvar ) {
 	}
 	vmCvar->modificationCount = cv->modificationCount;
 	if ( strlen(cv->string)+1 > MAX_CVAR_VALUE_STRING ) 
-	  Com_Error( ERR_DROP, "Cvar_Update: src %s length %d exceeds MAX_CVAR_VALUE_STRING",
+	  Com_Error( ERR_DROP, "Cvar_Update: src %s length %u exceeds MAX_CVAR_VALUE_STRING",
 		     cv->string, 
-		     strlen(cv->string));
+		     (unsigned int) strlen(cv->string));
 	Q_strncpyz( vmCvar->string, cv->string,  MAX_CVAR_VALUE_STRING ); 
 
 	vmCvar->value = cv->value;
