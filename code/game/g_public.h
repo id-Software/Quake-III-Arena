657c791257535324d7199bbd28ee8d76ffcd7501
Thilo Schulz <arny@ats.s.bawue.de>
2011-03-08 01:37:28 +0000

    Zack Middleton made me do it

--- code/game/g_public.h
+++ code/game/g_public.h
@@ -83,7 +83,7 @@ typedef struct {
 
 	// when a trace call is made and passEntityNum != ENTITYNUM_NONE,
 	// an ent will be excluded from testing if:
-	// ent->s.number == passEntityNum	(don't interact with self)
+	// ent->r.number == passEntityNum	(don't interact with self)
 	// ent->r.ownerNum == passEntityNum	(don't interact with your own missiles)
 	// entity[ent->r.ownerNum].r.ownerNum == passEntityNum	(don't interact with other missiles from owner)
 	int			ownerNum;
