d3f8dffe39a0c495582c646684ebd63d528fddb3
Thilo Schulz <arny@ats.s.bawue.de>
2011-03-17 12:53:47 +0000

    Bug 4936 - Wrong color for non-ascii symbols in console

--- code/client/cl_console.c
+++ code/client/cl_console.c
@@ -380,9 +380,9 @@ If no console is visible, the text will appear at the top of the game window
 ================
 */
 void CL_ConsolePrint( char *txt ) {
-	int		y;
-	int		c, l;
-	int		color;
+	int		y, l;
+	unsigned char	c;
+	unsigned short	color;
 	qboolean skipnotify = qfalse;		// NERVE - SMF
 	int prev;							// NERVE - SMF
 
@@ -410,7 +410,7 @@ void CL_ConsolePrint( char *txt ) {
 
 	color = ColorIndex(COLOR_WHITE);
 
-	while ( (c = *txt) != 0 ) {
+	while ( (c = *((unsigned char *) txt)) != 0 ) {
 		if ( Q_IsColorString( txt ) ) {
 			color = ColorIndex( *(txt+1) );
 			txt += 2;
