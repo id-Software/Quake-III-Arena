3551b0fa3cde34efa751cba86813c44854d59c17
Thilo Schulz <arny@ats.s.bawue.de>
2011-04-18 18:55:58 +0000

    Let codec loader decide which extension to give to extensionless filenames

--- code/client/snd_main.c
+++ code/client/snd_main.c
@@ -410,12 +410,9 @@ void S_Play_f( void ) {
 
 	i = 1;
 	while ( i<Cmd_Argc() ) {
-		if ( !Q_strrchr(Cmd_Argv(i), '.') ) {
-			Com_sprintf( name, sizeof(name), "%s.wav", Cmd_Argv(1) );
-		} else {
-			Q_strncpyz( name, Cmd_Argv(i), sizeof(name) );
-		}
+		Q_strncpyz( name, Cmd_Argv(i), sizeof(name) );
 		h = si.RegisterSound( name, qfalse );
+
 		if( h ) {
 			si.StartLocalSound( h, CHAN_LOCAL_SOUND );
 		}
