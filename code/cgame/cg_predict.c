b921058a9fe482ced405d6651eb0481b3cf17452
Tim Angus <tim@ngus.net>
2011-06-06 16:55:38 +0000

    * Remove misleading comment

--- code/cgame/cg_predict.c
+++ code/cgame/cg_predict.c
@@ -186,7 +186,6 @@ int		CG_PointContents( const vec3_t point, int passEntityNum ) {
 			continue;
 		}
 
-		// Ensiform : Trivial change to use the projected origin & angles of moving water based entities.
 		contents |= trap_CM_TransformedPointContents( point, cmodel, cent->lerpOrigin, cent->lerpAngles );
 	}
 
