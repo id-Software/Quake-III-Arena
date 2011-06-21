95691ded36c93d03f242626b14ba820f5d7f336e
Thilo Schulz <arny@ats.s.bawue.de>
2011-06-21 12:32:43 +0000

    Fix x64 compilation on MSVC

--- code/qcommon/vm_x86.c
+++ code/qcommon/vm_x86.c
@@ -413,7 +413,8 @@ static void DoSyscall(void)
 
 	vm_t *savedVM;
 
-#if defined(_MSC_VER) && !idx64
+#if defined(_MSC_VER)
+  #if !idx64
 	__asm
 	{
 		mov	dword ptr syscallNum, eax
@@ -422,6 +423,7 @@ static void DoSyscall(void)
 		mov	dword ptr opStackBase, edi
 		mov	dword ptr arg, ecx
 	}
+  #endif
 #else
 	__asm__ volatile(
 		""
