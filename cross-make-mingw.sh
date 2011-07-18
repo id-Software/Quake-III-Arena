59e9bb9ed37a2962143499e2407b72a41f962ca1
Thilo Schulz <arny@ats.s.bawue.de>
2011-07-18 22:27:25 +0000

    Bug 5084 - [patch] add support for Debian cross compiling, by q3urt.undead@gmail.com

--- cross-make-mingw.sh
+++ cross-make-mingw.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 
-if [ !$CC ]
-then
-  export CC=i586-mingw32msvc-gcc
+CMD_PREFIX="i586-mingw32msvc i686-w64-mingw32";
+
+if [ "X$CC" = "X" ]; then
+    for check in $CMD_PREFIX; do
+        full_check="${check}-gcc"
+	if [ ! $(which "$full_check") = "" ]; then
+	    export CC="$full_check"
+	fi
+    done
 fi
 
-if [ !$WINDRES ]
-then
-  export WINDRES=i586-mingw32msvc-windres
+if [ "X$WINDRES" = "X" ]; then
+    for check in $CMD_PREFIX; do
+        full_check="${check}-windres"
+	if [ ! $(which "$full_check") = "" ]; then
+	    export WINDRES="$full_check"
+	fi
+    done
+fi
+
+if [ "X$WINDRES" = "X" -o "X$CC" = "X" ]; then
+    echo "Error: Must define or find WINDRES and CC"
+    exit 1
 fi
 
 export PLATFORM=mingw32
