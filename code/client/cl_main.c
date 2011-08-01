c21dee0b37ad8be84519bf4ff797493dcf6b5e51
Thilo Schulz <arny@ats.s.bawue.de>
2011-08-01 14:40:53 +0000

    [16:31:51] <ZTurtleMan> Thilo: two small fixes, one for r2112 and one for r2116. http://pastebin.com/raw.php?i=h19r211Z

--- code/client/cl_main.c
+++ code/client/cl_main.c
@@ -47,7 +47,9 @@ cvar_t	*cl_voipShowMeter;
 cvar_t	*cl_voip;
 #endif
 
+#ifdef USE_RENDERER_DLOPEN
 cvar_t	*cl_renderer;
+#endif
 
 cvar_t	*cl_nodelta;
 cvar_t	*cl_debugMove;
--- code/game/bg_pmove.c
+++ code/game/bg_pmove.c
@@ -1862,7 +1862,7 @@ void PmoveSingle (pmove_t *pmove) {
 	}
 
 	// set the firing flag for continuous beam weapons
-	if ( !(pm->ps->pm_flags & PMF_RESPAWNED) && pm->ps->pm_type != PM_INTERMISSION && (!pm->ps->pm_type == PM_NOCLIP)
+	if ( !(pm->ps->pm_flags & PMF_RESPAWNED) && pm->ps->pm_type != PM_INTERMISSION && pm->ps->pm_type != PM_NOCLIP
 		&& ( pm->cmd.buttons & BUTTON_ATTACK ) && pm->ps->ammo[ pm->ps->weapon ] ) {
 		pm->ps->eFlags |= EF_FIRING;
 	} else {
