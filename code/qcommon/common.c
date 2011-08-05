5d24905c8d1c3b897c3c99da28259bbc7fd4bdfa
Zack Middleton <zturtleman@gmail.com>
2011-08-05 21:45:22 +0000

    Simulate line buffering and fix the overflow bug in Com_ReadFromPipe(), patch from DevHC.

--- code/qcommon/common.c
+++ code/qcommon/common.c
@@ -2883,15 +2883,45 @@ Read whatever is in com_pipefile, if anything, and execute it
 */
 void Com_ReadFromPipe( void )
 {
-	char buffer[MAX_STRING_CHARS] = {""};
-	qboolean read;
+	static char buf[MAX_STRING_CHARS];
+	static int accu = 0;
+	int read;
 
 	if( !pipefile )
 		return;
 
-	read = FS_Read( buffer, sizeof( buffer ), pipefile );
-	if( read )
-		Cbuf_ExecuteText( EXEC_APPEND, buffer );
+	while( ( read = FS_Read( buf + accu, sizeof( buf ) - accu - 1, pipefile ) ) > 0 )
+	{
+		char *brk = NULL;
+		int i;
+
+		for( i = accu; i < accu + read; ++i )
+		{
+			if( buf[ i ] == '\0' )
+				buf[ i ] = '\n';
+			if( buf[ i ] == '\n' || buf[ i ] == '\r' )
+				brk = &buf[ i + 1 ];
+		}
+		buf[ accu + read ] = '\0';
+
+		accu += read;
+
+		if( brk )
+		{
+			char tmp = *brk;
+			*brk = '\0';
+			Cbuf_ExecuteText( EXEC_APPEND, buf );
+			*brk = tmp;
+
+			accu -= brk - buf;
+			memmove( buf, brk, accu + 1 );
+		}
+		else if( accu >= sizeof( buf ) - 1 ) // full
+		{
+			Cbuf_ExecuteText( EXEC_APPEND, buf );
+			accu = 0;
+		}
+	}
 }
 
 
