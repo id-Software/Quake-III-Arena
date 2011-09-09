b7fa3e70738a5d9acdd0e63f9b8d562bc6ebf26e
Zack Middleton <zturtleman@gmail.com>
2011-09-09 21:50:45 +0000

    Use EXEC_NOW instead of hardcoded 0 in cl_ui.c

--- code/client/cl_ui.c
+++ code/client/cl_ui.c
@@ -765,7 +765,7 @@ intptr_t CL_UISystemCalls( intptr_t *args ) {
 		return 0;
 
 	case UI_CMD_EXECUTETEXT:
-		if(args[1] == 0
+		if(args[1] == EXEC_NOW
 		&& (!strncmp(VMA(2), "snd_restart", 11)
 		|| !strncmp(VMA(2), "vid_restart", 11)
 		|| !strncmp(VMA(2), "quit", 5)))
