3774a8aeee969ef09ab6b66d81146d2dcacd090f
Zack Middleton <zturtleman@gmail.com>
2011-09-12 14:54:01 +0000

    Restored loading ".dat" journal files from disk when connect to pure servers. Accidentally broke in r1911, reported by Ensiform.

--- code/qcommon/files.c
+++ code/qcommon/files.c
@@ -1286,7 +1286,7 @@ long FS_FOpenFileReadDir(const char *filename, searchpath_t *search, fileHandle_
 			if(!FS_IsExt(filename, ".cfg", len) &&		// for config files
 			   !FS_IsExt(filename, ".menu", len) &&		// menu files
 			   !FS_IsExt(filename, ".game", len) &&		// menu files
-			   !FS_IsExt(filename, ".cfg", len) &&		// for journal files
+			   !FS_IsExt(filename, ".dat", len) &&		// for journal files
 			   !FS_IsDemoExt(filename, len))			// demos
 			{
 				*file = 0;
