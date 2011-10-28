4ccd548512149c0e07779af904b30274ad75c6a6
Tim Angus <tim@ngus.net>
2011-10-28 18:51:31 +0000

    * I zigged when I should have zagged

--- code/asm/snapvector.c
+++ code/asm/snapvector.c
@@ -55,9 +55,9 @@ void qsnapvectorsse(vec3_t vec)
 
 #define QROUNDX87(src) \
 	"flds " src "\n" \
-	"fistpl " src "\n" \
-	"fildl " src "\n" \
-	"fstpl " src "\n"	
+	"fistps " src "\n" \
+	"filds " src "\n" \
+	"fstps " src "\n"	
 
 void qsnapvectorx87(vec3_t vec)
 {
