fd0d156338e4c9f699b75f4906b5a753e52aa904
Zack Middleton <zturtleman@gmail.com>
2011-11-10 04:49:07 +0000

    Added missing newlines to game dedicated chat messages.

--- code/game/g_svcmds.c
+++ code/game/g_svcmds.c
@@ -495,11 +495,11 @@ qboolean	ConsoleCommand( void ) {
 
 	if (g_dedicated.integer) {
 		if (Q_stricmp (cmd, "say") == 0) {
-			trap_SendServerCommand( -1, va("print \"server: %s\"", ConcatArgs(1) ) );
+			trap_SendServerCommand( -1, va("print \"server: %s\n\"", ConcatArgs(1) ) );
 			return qtrue;
 		}
 		// everything else will also be printed as a say command
-		trap_SendServerCommand( -1, va("print \"server: %s\"", ConcatArgs(0) ) );
+		trap_SendServerCommand( -1, va("print \"server: %s\n\"", ConcatArgs(0) ) );
 		return qtrue;
 	}
 
