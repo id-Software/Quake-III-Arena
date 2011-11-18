3ecd92ed916e403c8c0a1918d8cfec08a87bf0e4
Thilo Schulz <arny@ats.s.bawue.de>
2011-11-18 12:47:42 +0000

    Add color combination green-magenta for anaglyph

--- code/renderer/tr_cmds.c
+++ code/renderer/tr_cmds.c
@@ -297,7 +297,8 @@ void RE_StretchPic ( float x, float y, float w, float h,
 #define MODE_RED_CYAN	1
 #define MODE_RED_BLUE	2
 #define MODE_RED_GREEN	3
-#define MODE_MAX	MODE_RED_GREEN
+#define MODE_GREEN_MAGENTA 4
+#define MODE_MAX	MODE_GREEN_MAGENTA
 
 void R_SetColorMode(GLboolean *rgba, stereoFrame_t stereoFrame, int colormode)
 {
@@ -313,16 +314,26 @@ void R_SetColorMode(GLboolean *rgba, stereoFrame_t stereoFrame, int colormode)
 		colormode -= MODE_MAX;
 	}
 	
-	if(stereoFrame == STEREO_LEFT)
-		rgba[1] = rgba[2] = GL_FALSE;
-	else if(stereoFrame == STEREO_RIGHT)
+	if(colormode == MODE_GREEN_MAGENTA)
 	{
-		rgba[0] = GL_FALSE;
-		
-		if(colormode == MODE_RED_BLUE)
+		if(stereoFrame == STEREO_LEFT)
+			rgba[0] = rgba[2] = GL_FALSE;
+		else if(stereoFrame == STEREO_RIGHT)
 			rgba[1] = GL_FALSE;
-		else if(colormode == MODE_RED_GREEN)
-			rgba[2] = GL_FALSE;
+	}
+	else
+	{
+		if(stereoFrame == STEREO_LEFT)
+			rgba[1] = rgba[2] = GL_FALSE;
+		else if(stereoFrame == STEREO_RIGHT)
+		{
+			rgba[0] = GL_FALSE;
+		
+			if(colormode == MODE_RED_BLUE)
+				rgba[1] = GL_FALSE;
+			else if(colormode == MODE_RED_GREEN)
+				rgba[2] = GL_FALSE;
+		}
 	}
 }
 
