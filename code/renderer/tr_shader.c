5abf7e3d846b72ecf81cf193256de924ff8b6113
Zack Middleton <zturtleman@gmail.com>
2012-05-07 23:06:00 +0000

    #5503 - SIGSEGV with r_vertexlight 1 in missionpack (patch by Serge Belyshev).

--- code/renderer/tr_shader.c
+++ code/renderer/tr_shader.c
@@ -627,7 +627,7 @@ static qboolean ParseStage( shaderStage_t *stage, char **text )
 			else if ( !Q_stricmp( token, "$lightmap" ) )
 			{
 				stage->bundle[0].isLightmap = qtrue;
-				if ( shader.lightmapIndex < 0 ) {
+				if ( shader.lightmapIndex < 0 || !tr.lightmaps ) {
 					stage->bundle[0].image[0] = tr.whiteImage;
 				} else {
 					stage->bundle[0].image[0] = tr.lightmaps[shader.lightmapIndex];
