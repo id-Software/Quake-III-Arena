8534c94e4c4cf9a755bf5cd0fe0f01570bbd769c
Thilo Schulz <arny@ats.s.bawue.de>
2012-07-01 16:29:43 +0000

    Support building on cygwin with cross-make-mingw*.sh (#5405) - patch by q3urt.undead@gmail.com

--- cross-make-mingw.sh
+++ cross-make-mingw.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
+# Note: This works in Linux and cygwin
+
 CMD_PREFIX="i586-mingw32msvc i686-w64-mingw32";
 
 if [ "X$CC" = "X" ]; then
     for check in $CMD_PREFIX; do
         full_check="${check}-gcc"
-	if [ ! $(which "$full_check") = "" ]; then
-	    export CC="$full_check"
-	fi
+        which "$full_check" > /dev/null 2>&1
+        if [ "$?" = "0" ]; then
+            export CC="$full_check"
+        fi
     done
 fi
 
 if [ "X$WINDRES" = "X" ]; then
     for check in $CMD_PREFIX; do
         full_check="${check}-windres"
-	if [ ! $(which "$full_check") = "" ]; then
-	    export WINDRES="$full_check"
-	fi
+        which "$full_check" > /dev/null 2>&1
+        if [ "$?" = "0" ]; then
+            export WINDRES="$full_check"
+        fi
     done
 fi
 
