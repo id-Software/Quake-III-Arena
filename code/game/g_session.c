56ebe14239ed5fd039b72494c6fa2222b4fc9a37
Zack Middleton <zturtleman@gmail.com>
2012-09-08 22:32:41 +0000

    Don't have g_teamAutoJoin affect bot's team
    Bots set their team later, setting it here causes some bots to change team and die later. Can cause extra skulls to be spawn at beginning of harvester (bug #5740).

--- code/game/g_session.c
+++ code/game/g_session.c
@@ -107,7 +107,7 @@ void G_InitSessionData( gclient_t *client, char *userinfo ) {
 
 	// initial team determination
 	if ( g_gametype.integer >= GT_TEAM ) {
-		if ( g_teamAutoJoin.integer ) {
+		if ( g_teamAutoJoin.integer && !(g_entities[ client - level.clients ].r.svFlags & SVF_BOT) ) {
 			sess->sessionTeam = PickTeam( -1 );
 			BroadcastTeamChange( client, -1 );
 		} else {
