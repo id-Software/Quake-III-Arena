c65142179f8ba64c8354e0e898cf2ede73c6b14c
Zack Middleton <zturtleman@gmail.com>
2012-09-09 22:14:15 +0000

    Fix g_warmup < 2 causing client prediction issues (bug #5740). (Reused code for tournament gametype.)

--- code/game/g_main.c
+++ code/game/g_main.c
@@ -1522,7 +1522,12 @@ void CheckTournament( void ) {
 		// if all players have arrived, start the countdown
 		if ( level.warmupTime < 0 ) {
 			// fudge by -1 to account for extra delays
-			level.warmupTime = level.time + ( g_warmup.integer - 1 ) * 1000;
+			if ( g_warmup.integer > 1 ) {
+				level.warmupTime = level.time + ( g_warmup.integer - 1 ) * 1000;
+			} else {
+				level.warmupTime = 0;
+			}
+
 			trap_SetConfigstring( CS_WARMUP, va("%i", level.warmupTime) );
 			return;
 		}
