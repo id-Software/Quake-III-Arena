7786f95c066eb55411bcdca43cd14c4a1c776b2f
Zack Middleton <zturtleman@gmail.com>
2012-12-13 05:25:50 +0000

    Fix writting voip data in demos (broke in r2102).

--- code/client/cl_input.c
+++ code/client/cl_input.c
@@ -815,6 +815,7 @@ void CL_WritePacket( void ) {
 				MSG_WriteLong (&fakemsg, clc.voipOutgoingSequence);
 				MSG_WriteByte (&fakemsg, clc.voipOutgoingDataFrames);
 				MSG_WriteShort (&fakemsg, clc.voipOutgoingDataSize );
+				MSG_WriteBits (&fakemsg, clc.voipFlags, VOIP_FLAGCNT);
 				MSG_WriteData (&fakemsg, clc.voipOutgoingData, voipSize);
 				MSG_WriteByte (&fakemsg, svc_EOF);
 				CL_WriteDemoMessage (&fakemsg, 0);
