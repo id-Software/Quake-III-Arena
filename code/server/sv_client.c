b943d506bf3dd074f8637a8fcedd533c26c07e9a
Zack Middleton <zturtleman@gmail.com>
2012-12-15 08:08:46 +0000

    Fix clients being able to reset their player state and respawn using donedl.

--- code/server/sv_client.c
+++ code/server/sv_client.c
@@ -853,6 +853,9 @@ Downloads are finished
 ==================
 */
 static void SV_DoneDownload_f( client_t *cl ) {
+	if ( cl->state == CS_ACTIVE )
+		return;
+
 	Com_DPrintf( "clientDownload: %s Done\n", cl->name);
 	// resend the game state to update any clients that entered during the download
 	SV_SendClientGameState(cl);
