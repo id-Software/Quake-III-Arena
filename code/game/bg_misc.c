4de32b1c1af6ced13e1ebcef7aa9662b513ee91f
Jeremy Davis <ensiform@gmail.com>
2013-01-14 14:50:12 -0600

    Fix error message in BG_EvaluateTrajectory

--- code/game/bg_misc.c
+++ code/game/bg_misc.c
@@ -1229,7 +1229,7 @@ void BG_EvaluateTrajectory( const trajectory_t *tr, int atTime, vec3_t result ) 
 		result[2] -= 0.5 * DEFAULT_GRAVITY * deltaTime * deltaTime;		// FIXME: local gravity...
 		break;
 	default:
-		Com_Error( ERR_DROP, "BG_EvaluateTrajectory: unknown trType: %i", tr->trTime );
+		Com_Error( ERR_DROP, "BG_EvaluateTrajectory: unknown trType: %i", tr->trType );
 		break;
 	}
 }
