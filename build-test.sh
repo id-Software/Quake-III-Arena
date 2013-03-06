361c71f8afb4bd50b75bcdc84ceee1faaa2cddc8
Zack Middleton <zturtleman@gmail.com>
2013-03-06 17:24:59 -0600

    autobuild: try to fix extra options build and mingw builds

--- build-test.sh
+++ build-test.sh
@@ -6,10 +6,15 @@ failed=0;
 (make) || failed=1;
 
 # Test additional options
+make clean
 (make USE_CODEC_VORBIS=1 USE_FREETYPE=1 CFLAGS=-DRAVENMD4) || failed=1;
 
-# Test mingw
-(exec ./cross-make-mingw.sh) || failed=1;
+# Test mingw (gcc)
+if [ "$CC" = "gcc" ]; then
+	# clear CC so script will set it.
+    export CC=
+    (exec ./cross-make-mingw.sh) || failed=1;
+fi
 
 if [ $failed -eq 1 ]; then
   echo "Build failure.";
