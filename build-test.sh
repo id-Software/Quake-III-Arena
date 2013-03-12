71632ea29185fbd73bd1ecca7d9ab92b33cd7bed
Zack Middleton <zturtleman@gmail.com>
2013-03-12 18:52:36 -0500

    Disable freetype and vorbis for mingw travis build
    Libs are missing.

--- build-test.sh
+++ build-test.sh
@@ -5,15 +5,21 @@ failed=0;
 # check if testing mingw
 if [ "$CC" = "i686-w64-mingw32-gcc" ]; then
 	MAKE=./cross-make-mingw.sh
+	haveExternalLibs=0
 else
 	MAKE=make
+	haveExternalLibs=1
 fi
 
 # Default Build
 ($MAKE clean release) || failed=1;
 
 # Test additional options
-($MAKE clean release USE_CODEC_VORBIS=1 USE_FREETYPE=1 CFLAGS=-DRAVENMD4) || failed=1;
+if [ $haveExternalLibs -eq 1 ]; then
+	($MAKE clean release USE_CODEC_VORBIS=1 USE_FREETYPE=1 CFLAGS=-DRAVENMD4) || failed=1;
+else
+	($MAKE clean release CFLAGS=-DRAVENMD4) || failed=1;
+fi
 
 if [ $failed -eq 1 ]; then
 	echo "Build failure.";
