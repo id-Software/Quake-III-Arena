a2dc160359c5acfe96d98b0263d3f6eb4d6e78f3
Zack Middleton <zturtleman@gmail.com>
2013-03-12 16:33:49 -0500

    Move mingw to separate build job on travis-ci

--- build-test.sh
+++ build-test.sh
@@ -2,21 +2,18 @@
 
 failed=0;
 
+# check if testing mingw
+if [ "$CC" = "i686-w64-mingw-gcc" ]; then
+	MAKE=./cross-make-mingw.sh
+else
+	MAKE=make
+fi
+
 # Default Build
-(make clean release) || failed=1;
+($MAKE clean release) || failed=1;
 
 # Test additional options
-(make clean release USE_CODEC_VORBIS=1 USE_FREETYPE=1 CFLAGS=-DRAVENMD4) || failed=1;
-
-# Test mingw
-if [ "$CC" = "clang" ]; then
-	# skip mingw if travis-ci clang build
-	echo "Skipping mingw build because there is no mingw clang compiler available.";
-else
-	# clear CC so cross-make-mingw script will set it.
-	export CC=
-	(exec ./cross-make-mingw.sh clean release) || failed=1;
-fi
+($MAKE clean release USE_CODEC_VORBIS=1 USE_FREETYPE=1 CFLAGS=-DRAVENMD4) || failed=1;
 
 if [ $failed -eq 1 ]; then
 	echo "Build failure.";
