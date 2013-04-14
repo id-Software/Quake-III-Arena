830d93aa38dc50d40dccab35689ade70ebd664bc
/dev/humancontroller <devhc@example.com>
2013-04-14 18:33:24 +0200

    add some noreturn annotations

--- code/qcommon/common.c
+++ code/qcommon/common.c
@@ -2248,7 +2248,7 @@ Just throw a fatal error to
 test error shutdown procedures
 =============
 */
-static void Com_Error_f (void) {
+static void __attribute__((__noreturn__)) Com_Error_f (void) {
 	if ( Cmd_Argc() > 1 ) {
 		Com_Error( ERR_DROP, "Testing drop error" );
 	} else {
