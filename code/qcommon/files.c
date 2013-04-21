a7317ac0d5d39ddb60364f21c2fd792be617a145
Zack Middleton <zturtleman@gmail.com>
2013-04-20 21:28:21 -0500

    Add fallback for __func__ (ioq3 isn't compiled as c99 by default)
    Using Debian gcc version 4.7.2 and clang 3.0-6.1 anyway.

--- code/qcommon/files.c
+++ code/qcommon/files.c
@@ -309,8 +309,12 @@ FILE*		missingFiles = NULL;
 #endif
 
 /* C99 defines __func__ */
-#ifndef __func__
-#define __func__ "(unknown)"
+#if __STDC_VERSION__ < 199901L
+#  if __GNUC__ >= 2 || _MSC_VER >= 1300
+#    define __func__ __FUNCTION__
+#  else
+#    define __func__ "(unknown)"
+#  endif
 #endif
 
 /*
