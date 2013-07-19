db85b61e9415c4655899aef9cff0d859c4ecf363
Henry Stratmann III <henry3writes@gmail.com>
2013-07-18 20:11:23 -0500

    Add assignment below declarations otherwise compiling fails with msvc
    Signed-off-by: Zack Middleton <zturtleman@gmail.com>

--- code/renderergl2/tr_bsp.c
+++ code/renderergl2/tr_bsp.c
@@ -3356,11 +3356,11 @@ void RE_LoadWorldMap( const char *name ) {
 	if (0)
 	{
 		world_t	*w;
-
-		w = &s_worldData;
 		uint8_t *primaryLightGrid, *data;
 		int lightGridSize;
 		int i;
+
+		w = &s_worldData;
 
 		lightGridSize = w->lightGridBounds[0] * w->lightGridBounds[1] * w->lightGridBounds[2];
 		primaryLightGrid = ri.Malloc(lightGridSize * sizeof(*primaryLightGrid));
