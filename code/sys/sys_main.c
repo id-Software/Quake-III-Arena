5fdff01d1bcd400d6fe319f466f8ec30d1624830
Zack Middleton <zturtleman@gmail.com>
2013-10-20 13:58:30 -0500

    Check #ifdef MACOS_X not #if MACOS_X

--- code/sys/sys_main.c
+++ code/sys/sys_main.c
@@ -613,7 +613,7 @@ int main( int argc, char **argv )
 	// Set the initial time base
 	Sys_Milliseconds( );
 
-#if MACOS_X
+#ifdef MACOS_X
 	// This is passed if we are launched by double-clicking
 	if ( argc >= 2 && Q_strncmp ( argv[1], "-psn", 4 ) == 0 )
 		argc = 1;
