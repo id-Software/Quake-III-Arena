a53e7c3074543fcbccb0e2be4d0121e3b7f9d1e1
Zack Middleton <zturtleman@gmail.com>
2013-11-11 01:12:42 -0600

    Fix comparing unsigned values to < 0 in gl2

--- code/renderergl2/tr_bsp.c
+++ code/renderergl2/tr_bsp.c
@@ -746,7 +746,7 @@ static void ParseFace( dsurface_t *ds, drawVert_t *verts, float *hdrVertColors, 
 		{
 			tri[j] = LittleLong(indexes[i + j]);
 
-			if(tri[j] < 0 || tri[j] >= numVerts)
+			if(tri[j] >= numVerts)
 			{
 				ri.Error(ERR_DROP, "Bad index in face surface");
 			}
@@ -990,7 +990,7 @@ static void ParseTriSurf( dsurface_t *ds, drawVert_t *verts, float *hdrVertColor
 		{
 			tri[j] = LittleLong(indexes[i + j]);
 
-			if(tri[j] < 0 || tri[j] >= numVerts)
+			if(tri[j] >= numVerts)
 			{
 				ri.Error(ERR_DROP, "Bad index in face surface");
 			}
