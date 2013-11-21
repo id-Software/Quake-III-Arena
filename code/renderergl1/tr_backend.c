8af9516e88b7b5c4731e4770f053a0a8ea7a45ac
Zack Middleton <zturtleman@gmail.com>
2013-11-20 21:37:17 -0600

    End current draw surface before drawing cinematic
    Fixes drawing StretchPic *before* CIN_DrawCinematic resulting
    in cinematic being drawn before the StretchPic.

--- code/renderergl1/tr_backend.c
+++ code/renderergl1/tr_backend.c
@@ -737,6 +737,10 @@ void RE_StretchRaw (int x, int y, int w, int h, int cols, int rows, const byte *
 	}
 	R_IssuePendingRenderCommands();
 
+	if ( tess.numIndexes ) {
+		RB_EndSurface();
+	}
+
 	// we definately want to sync every frame for the cinematics
 	qglFinish();
 
