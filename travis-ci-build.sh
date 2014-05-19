494789664e3c958efd964d722556a56cb0cc0268
Zack Middleton <zturtleman@gmail.com>
2014-05-19 17:27:25 -0500

    Try to fix Travis CI MinGW builds
    Listing i686-w64-mingw32-gcc as a compiler stopped working.
    Travis tries to print version before installing it.
    
    $ i686-w64-mingw32-gcc --version
    
    /home/travis/build.sh: line 201: i686-w64-mingw32-gcc: command not found
    
    The command "i686-w64-mingw32-gcc --version" failed and exited with 127 during setup.

--- travis-ci-build.sh
+++ travis-ci-build.sh
@@ -2,33 +2,13 @@
 
 failed=0;
 
-# check if testing mingw
-if [ "$CC" = "i686-w64-mingw32-gcc" ]; then
-	export PLATFORM=mingw32
-	export ARCH=x86
-	export CC=
-	haveExternalLibs=0
-elif [ "$CC" = "x86_64-w64-mingw32-gcc" ]; then
-	export PLATFORM=mingw32
-	export ARCH=x86_64
-	export CC=
-	haveExternalLibs=0
-else
-	haveExternalLibs=1
-fi
-
 # Default Build
 (make clean release) || failed=1;
-
-# Test additional options
-if [ $haveExternalLibs -eq 1 ]; then
-	(make clean release USE_CODEC_VORBIS=1 USE_FREETYPE=1) || failed=1;
-fi
 
 if [ $failed -eq 1 ]; then
 	echo "Build failure.";
 else
-	echo "All builds successful.";
+	echo "Build successful.";
 fi
 
 exit $failed;
