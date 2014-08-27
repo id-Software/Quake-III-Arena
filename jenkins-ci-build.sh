19ee6a86eb785c4f6c464ac57d91b6fe4b8a6138
Tim Angus <tim.angus@roslin.ed.ac.uk>
2014-08-27 16:48:55 +0100

    Add cppcheck stage to jenkins script

--- jenkins-ci-build.sh
+++ jenkins-ci-build.sh
@@ -8,8 +8,8 @@ cd ${MASTER_DIR}
 
 if [ "$OPTIONS" = "all_options" ];
 then
-    export USE_CODEC_VORBIS=1
-    export USE_FREETYPE=1
+	export USE_CODEC_VORBIS=1
+	export USE_FREETYPE=1
 fi
 
 if [ "$UNAME" == "Darwin" ]; then
@@ -25,6 +25,22 @@ if [ "${BUILD_TYPE}" == "" ]; then
 	echo "build type    : defaulting to ${BUILD_TYPE}"
 else
 	echo "build type    : ${BUILD_TYPE}"
+fi
+
+if [ -n "${CPPCHECK}" ]; then
+	if [ -e "${CPPCHECK}" ]; then
+		# Copy the existing output
+		BASENAME_CPPCHECK=`basename ${CPPCHECK}`
+		cp ${CPPCHECK} ./${BASENAME_CPPCHECK}
+	else
+		CHECK_CPPCHECK=`command -v cppcheck >/dev/null`
+		if [ "$?" != "0" ]; then
+			echo "cppcheck not installed"
+			exit 1
+		fi
+
+		cppcheck --enable=all --max-configs=1 --xml --xml-version=2 ./code 2> ${CPPCHECK}
+	fi
 fi
 
 make -j${CORES} distclean ${BUILD_TYPE}
