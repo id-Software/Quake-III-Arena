e5f54c584acded3092f3d9dbdd3cd0c0a5ad12d3
Zack Middleton <zturtleman@gmail.com>
2014-08-27 20:27:30 -0500

    Fix incorrect rgbGen const reading uninitialized memory
    If ParseVector fails, color isn't fully set.

--- code/renderergl1/tr_shader.c
+++ code/renderergl1/tr_shader.c
@@ -847,6 +847,8 @@ static qboolean ParseStage( shaderStage_t *stage, char **text )
 			{
 				vec3_t	color;
 
+				VectorClear( color );
+
 				ParseVector( text, 3, color );
 				stage->constantColor[0] = 255 * color[0];
 				stage->constantColor[1] = 255 * color[1];
