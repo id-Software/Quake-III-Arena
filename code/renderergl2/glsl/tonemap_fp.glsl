7e808f92d62dbf0c29997315dc7aeb69cc372b86
SmileTheory <SmileTheory@gmail.com>
2014-10-27 06:37:57 -0700

    OpenGL2: Speed up tonemap shader.

--- code/renderergl2/glsl/tonemap_fp.glsl
+++ code/renderergl2/glsl/tonemap_fp.glsl
@@ -3,14 +3,16 @@ uniform sampler2D u_LevelsMap;
 
 uniform vec4      u_Color;
 
+
 uniform vec2      u_AutoExposureMinMax;
 uniform vec3      u_ToneMinAvgMaxLinear;
 
 varying vec2      var_TexCoords;
+varying float     var_InvWhite;
 
 const vec3  LUMINANCE_VECTOR =   vec3(0.2125, 0.7154, 0.0721); //vec3(0.299, 0.587, 0.114);
 
-vec3 FilmicTonemap(vec3 x)
+float FilmicTonemap(float x)
 {
 	const float SS  = 0.22; // Shoulder Strength
 	const float LS  = 0.30; // Linear Strength
@@ -18,40 +20,35 @@ vec3 FilmicTonemap(vec3 x)
 	const float TS  = 0.20; // Toe Strength
 	const float TAN = 0.01; // Toe Angle Numerator
 	const float TAD = 0.30; // Toe Angle Denominator
-	
-	vec3 SSxx = SS * x * x;
-	vec3 LSx = LS * x;
-	vec3 LALSx = LSx * LA;
-	
-	return ((SSxx + LALSx + TS * TAN) / (SSxx + LSx + TS * TAD)) - TAN / TAD;
 
-	//return ((x*(SS*x+LA*LS)+TS*TAN)/(x*(SS*x+LS)+TS*TAD)) - TAN/TAD;
-
+	return ((x*(SS*x+LA*LS)+TS*TAN)/(x*(SS*x+LS)+TS*TAD)) - TAN/TAD;
 }
 
 void main()
 {
 	vec4 color = texture2D(u_TextureMap, var_TexCoords) * u_Color;
 
-  #if defined(r_framebufferGamma)
+#if defined(r_framebufferGamma)
 	color.rgb = pow(color.rgb, vec3(r_framebufferGamma));
-  #endif
+#endif
 
 	vec3 minAvgMax = texture2D(u_LevelsMap, var_TexCoords).rgb;
 	vec3 logMinAvgMaxLum = clamp(minAvgMax * 20.0 - 10.0, -u_AutoExposureMinMax.y, -u_AutoExposureMinMax.x);
-		
-	float avgLum = exp2(logMinAvgMaxLum.y);
-	//float maxLum = exp2(logMinAvgMaxLum.z);
 
-	color.rgb *= u_ToneMinAvgMaxLinear.y / avgLum;
-	color.rgb = max(vec3(0.0), color.rgb - vec3(u_ToneMinAvgMaxLinear.x));
+	float invAvgLum = u_ToneMinAvgMaxLinear.y * exp2(-logMinAvgMaxLum.y);
 
-	vec3 fWhite = 1.0 / FilmicTonemap(vec3(u_ToneMinAvgMaxLinear.z - u_ToneMinAvgMaxLinear.x));
-	color.rgb = FilmicTonemap(color.rgb) * fWhite;
-	
-  #if defined(r_tonemapGamma)
+	color.rgb = color.rgb * invAvgLum - u_ToneMinAvgMaxLinear.xxx;
+	color.rgb = max(vec3(0.0), color.rgb);
+
+	color.r = FilmicTonemap(color.r);
+	color.g = FilmicTonemap(color.g);
+	color.b = FilmicTonemap(color.b);
+
+	color.rgb = clamp(color.rgb * var_InvWhite, 0.0, 1.0);
+
+#if defined(r_tonemapGamma)
 	color.rgb = pow(color.rgb, vec3(1.0 / r_tonemapGamma));
-  #endif
-	
-	gl_FragColor = clamp(color, 0.0, 1.0);
+#endif
+
+	gl_FragColor = color;
 }
