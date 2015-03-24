1897afbd3af401fac8563497137af0b64ad0ed50
Zack Middleton <zturtleman@gmail.com>
2015-03-23 19:33:38 -0500

    Fix crosshair drawing not clearing color
    The renderer color is set to health color when drawing crosshair. After
    drawing the crosshair, the renderer color was not cleared and could affect
    other things. With cg_draw3dicons 0 and cg_drawCrosshairNames 0 it affected
    the attacker icon.

--- code/cgame/cg_draw.c
+++ code/cgame/cg_draw.c
@@ -1899,6 +1899,8 @@ static void CG_DrawCrosshair(void)
 	trap_R_DrawStretchPic( x + cg.refdef.x + 0.5 * (cg.refdef.width - w), 
 		y + cg.refdef.y + 0.5 * (cg.refdef.height - h), 
 		w, h, 0, 0, 1, 1, hShader );
+
+	trap_R_SetColor( NULL );
 }
 
 /*
