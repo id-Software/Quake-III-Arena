70eb1fa79e226197d68fb58e694f18ab3b24086f
Zack Middleton <zturtleman@gmail.com>
2015-06-28 18:02:48 -0500

    Make cinematic times be int like CL_ScaledMilliseconds
    Cinematic's startTime and lastTime are always set from CL_ScaledMilliseconds
    which returns int and are converted back and forth to int and unsigned int.
    
    This fixes a warning that abs() is used on an unsigned int.

--- code/client/cl_cin.c
+++ code/client/cl_cin.c
@@ -94,8 +94,8 @@ typedef struct {
 	qboolean			looping, holdAtEnd, dirty, alterGameState, silent, shader;
 	fileHandle_t		iFile;
 	e_status			status;
-	unsigned int		startTime;
-	unsigned int		lastTime;
+	int					startTime;
+	int					lastTime;
 	long				tfps;
 	long				RoQPlayed;
 	long				ROQSize;
