2a38bca9312cfbfcd2a36409c48263ecf3cb643d
Pan7 <panter@gmx.net>
2015-07-24 22:56:00 +0200

    SDL_Init zero check

--- code/sdl/sdl_snd.c
+++ code/sdl/sdl_snd.c
@@ -156,7 +156,7 @@ qboolean SNDDMA_Init(void)
 
 	if (!SDL_WasInit(SDL_INIT_AUDIO))
 	{
-		if (SDL_Init(SDL_INIT_AUDIO) == -1)
+		if (SDL_Init(SDL_INIT_AUDIO) != 0)
 		{
 			Com_Printf( "FAILED (%s)\n", SDL_GetError( ) );
 			return qfalse;
