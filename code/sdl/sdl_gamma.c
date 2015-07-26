19c4048315ce54dd20303a10c88e3b2f5fdfceb1
Pan7 <panter@gmx.net>
2015-07-26 15:45:21 +0200

    Added SDL_GetError()

--- code/sdl/sdl_gamma.c
+++ code/sdl/sdl_gamma.c
@@ -90,7 +90,7 @@ void GLimp_SetGamma( unsigned char red[256], unsigned char green[256], unsigned 
 
 	if (SDL_SetWindowGammaRamp(SDL_window, table[0], table[1], table[2]) < 0)
 	{
-		ri.Printf( PRINT_DEVELOPER, "SDL_SetWindowGammaRamp() failed.\n" );
+		ri.Printf( PRINT_DEVELOPER, "SDL_SetWindowGammaRamp() failed: %s\n", SDL_GetError() );
 	}
 }
 
