49ab0cc05e61a6c1b5862aa8b9495cee4a85b4cc
Tobias Kuehnhammer <kuehnhammertobias@gmail.com>
2015-07-26 19:55:17 +0200

    Fix (unused) watertype checks in PM_WaterMove

--- code/game/bg_pmove.c
+++ code/game/bg_pmove.c
@@ -483,9 +483,9 @@ static void PM_WaterMove( void ) {
 	// jump = head for surface
 	if ( pm->cmd.upmove >= 10 ) {
 		if (pm->ps->velocity[2] > -300) {
-			if ( pm->watertype == CONTENTS_WATER ) {
+			if ( pm->watertype & CONTENTS_WATER ) {
 				pm->ps->velocity[2] = 100;
-			} else if (pm->watertype == CONTENTS_SLIME) {
+			} else if ( pm->watertype & CONTENTS_SLIME ) {
 				pm->ps->velocity[2] = 80;
 			} else {
 				pm->ps->velocity[2] = 50;
