2a3c331ba1bfd31484f3f80d51c9d2b38f5d3191
Ryan C. Gordon <icculus@icculus.org>
2016-03-27 12:46:38 -0400

    Sys_GetProcessorFeatures() didn't check for 3DNow! or Altivec.

--- code/sys/sys_main.c
+++ code/sys/sys_main.c
@@ -274,10 +274,12 @@ cpuFeatures_t Sys_GetProcessorFeatures( void )
 	cpuFeatures_t features = 0;
 
 #ifndef DEDICATED
-	if( SDL_HasRDTSC( ) )    features |= CF_RDTSC;
-	if( SDL_HasMMX( ) )      features |= CF_MMX;
-	if( SDL_HasSSE( ) )      features |= CF_SSE;
-	if( SDL_HasSSE2( ) )     features |= CF_SSE2;
+	if( SDL_HasRDTSC( ) )      features |= CF_RDTSC;
+	if( SDL_SDL_Has3DNow( ) )  features |= CF_3DNOW;
+	if( SDL_HasMMX( ) )        features |= CF_MMX;
+	if( SDL_HasSSE( ) )        features |= CF_SSE;
+	if( SDL_HasSSE2( ) )       features |= CF_SSE2;
+	if( SDL_HasAltiVec( ) )    features |= CF_ALTIVEC;
 #endif
 
 	return features;
