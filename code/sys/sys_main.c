11668bb1f0fb2d70cd6760009f7781c079341a2a
Zack Middleton <zturtleman@gmail.com>
2016-03-27 12:02:12 -0500

    Fix typo of SDL_Has3DNow() in Sys_GetProcessorFeatures()

--- code/sys/sys_main.c
+++ code/sys/sys_main.c
@@ -275,7 +275,7 @@ cpuFeatures_t Sys_GetProcessorFeatures( void )
 
 #ifndef DEDICATED
 	if( SDL_HasRDTSC( ) )      features |= CF_RDTSC;
-	if( SDL_SDL_Has3DNow( ) )  features |= CF_3DNOW;
+	if( SDL_Has3DNow( ) )      features |= CF_3DNOW;
 	if( SDL_HasMMX( ) )        features |= CF_MMX;
 	if( SDL_HasSSE( ) )        features |= CF_SSE;
 	if( SDL_HasSSE2( ) )       features |= CF_SSE2;
