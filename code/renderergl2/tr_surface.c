ad819edcc488f766acbabb57844fff1c5aa8f2ea
SmileTheory <SmileTheory@gmail.com>
2016-10-11 04:18:41 -0700

    OpenGL2: Workaround for multiple deforms on MD3 mesh. https://github.com/ioquake/ioq3/issues/122

--- code/renderergl2/tr_surface.c
+++ code/renderergl2/tr_surface.c
@@ -1291,6 +1291,12 @@ void RB_SurfaceVaoMdvMesh(srfVaoMdvMesh_t * surface)
 
 	GLimp_LogComment("--- RB_SurfaceVaoMdvMesh ---\n");
 
+	if (ShaderRequiresCPUDeforms(tess.shader))
+	{
+		RB_SurfaceMesh(surface->mdvSurface);
+		return;
+	}
+
 	if(!surface->vao)
 		return;
 
