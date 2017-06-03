1066214548d6a154c05332c86610f60f03bd4193
Zack Middleton <zack@cloemail.com>
2017-06-02 21:40:21 -0500

    Fix "brought in 1 skulls" Harvester message
    Use correct singular/plural form of skulls for Harvester's brought in
    skulls message.
    
    Reported by Tobias.

--- code/game/g_team.c
+++ code/game/g_team.c
@@ -1280,8 +1280,8 @@ static void ObeliskTouch( gentity_t *self, gentity_t *other, trace_t *trace ) {
 		return;
 	}
 
-	PrintMsg(NULL, "%s" S_COLOR_WHITE " brought in %i skull%s.\n",
-					other->client->pers.netname, tokens, tokens ? "s" : "" );
+	PrintMsg(NULL, "%s" S_COLOR_WHITE " brought in %i %s.\n",
+					other->client->pers.netname, tokens, ( tokens == 1 ) ? "skull" : "skulls" );
 
 	AddTeamScore(self->s.pos.trBase, other->client->sess.sessionTeam, tokens);
 	Team_ForceGesture(other->client->sess.sessionTeam);
