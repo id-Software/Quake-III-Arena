06cc3a4e1bfcb3f2936ef5f4f0fec4aa7b6885e6
Ryan C. Gordon <icculus@icculus.org>
2017-06-04 01:44:39 -0400

    build-rsa-tools.sh now works on Linux.

--- code/autoupdater/rsa_tools/build-rsa-tools.sh
+++ code/autoupdater/rsa_tools/build-rsa-tools.sh
@@ -7,7 +7,11 @@ export TFMDIR="tomsfastmath-0.13.1"
 export LTCDIR="libtomcrypt-1.17"
 
 function build {
-    clang -mmacosx-version-min=10.7 -DMAC_OS_X_VERSION_MIN_REQUIRED=1070 -I $TFMDIR/src/headers -I $LTCDIR/src/headers -o "$1" -Wall -O3 "$1.c" rsa_common.c $LTCDIR/libtomcrypt.a $TFMDIR/libtfm.a
+    if [ "$OSTYPE" = "Darwin" ]; then
+        clang -mmacosx-version-min=10.7 -DMAC_OS_X_VERSION_MIN_REQUIRED=1070 -I $TFMDIR/src/headers -I $LTCDIR/src/headers -o "$1" -Wall -O3 "$1.c" rsa_common.c $LTCDIR/libtomcrypt.a $TFMDIR/libtfm.a
+    else
+        gcc -I $TFMDIR/src/headers -I $LTCDIR/src/headers -o "$1" -Wall -O3 "$1.c" rsa_common.c $LTCDIR/libtomcrypt.a $TFMDIR/libtfm.a
+    fi
 }
 
 set -e
