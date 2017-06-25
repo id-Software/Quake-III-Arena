7b9ccd14634df177280484db845ada007ea13be9
Zack Middleton <zack@cloemail.com>
2017-06-24 20:02:44 -0500

    Have spectator always be in first person
    Flying around in third person with no body is weird.

--- code/cgame/cg_view.c
+++ code/cgame/cg_view.c
@@ -796,7 +796,8 @@ void CG_DrawActiveFrame( int serverTime, stereoFrame_t stereoView, qboolean demo
 	CG_PredictPlayerState();
 
 	// decide on third person view
-	cg.renderingThirdPerson = cg_thirdPerson.integer || (cg.snap->ps.stats[STAT_HEALTH] <= 0);
+	cg.renderingThirdPerson = cg.snap->ps.persistant[PERS_TEAM] != TEAM_SPECTATOR
+							&& (cg_thirdPerson.integer || (cg.snap->ps.stats[STAT_HEALTH] <= 0));
 
 	// build cg.refdef
 	inwater = CG_CalcViewValues();
