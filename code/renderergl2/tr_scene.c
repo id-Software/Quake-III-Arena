09a23e04177202d197348104e1991f0bd622045a
Zack Middleton <zack@cloemail.com>
2017-06-29 21:34:39 -0500

    OpenGL2: Fix checking r_shadowCascadeZFar pointer instead of value

--- code/renderergl2/tr_scene.c
+++ code/renderergl2/tr_scene.c
@@ -491,7 +491,7 @@ void RE_RenderScene( const refdef_t *fd ) {
 	// playing with even more shadows
 	if(glRefConfig.framebufferObject && r_sunlightMode->integer && !( fd->rdflags & RDF_NOWORLDMODEL ) && (r_forceSun->integer || tr.sunShadows))
 	{
-		if (r_shadowCascadeZFar != 0)
+		if (r_shadowCascadeZFar->integer != 0)
 		{
 			R_RenderSunShadowMaps(fd, 0);
 			R_RenderSunShadowMaps(fd, 1);
