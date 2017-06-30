e77153766a2beff3b4fd4b115e52e0a617abb9c0
Zack Middleton <zack@cloemail.com>
2017-06-30 13:59:07 -0500

    OpenGL2: Draw sun shadows for first person IQM player models
    This makes IQM have the same behavior as MD3 and MDR.

--- code/renderergl2/tr_model_iqm.c
+++ code/renderergl2/tr_model_iqm.c
@@ -849,7 +849,8 @@ void R_AddIQMSurfaces( trRefEntity_t *ent ) {
 	surface = data->surfaces;
 
 	// don't add third_person objects if not in a portal
-	personalModel = (ent->e.renderfx & RF_THIRD_PERSON) && !tr.viewParms.isPortal;
+	personalModel = (ent->e.renderfx & RF_THIRD_PERSON) && !(tr.viewParms.isPortal
+	                 || (tr.viewParms.flags & (VPF_SHADOWMAP | VPF_DEPTHSHADOW)));
 
 	if ( ent->e.renderfx & RF_WRAP_FRAMES ) {
 		ent->e.frame %= data->num_frames;
