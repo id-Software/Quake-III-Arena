ce1d5406d5633d505efed2f0c57e859b51ec326d
Zack Middleton <zack@cloemail.com>
2018-05-28 17:20:27 -0500

    Require OpenGL 1.2 for GL_CLAMP_TO_EDGE

--- code/sdl/sdl_glimp.c
+++ code/sdl/sdl_glimp.c
@@ -266,7 +266,7 @@ static qboolean GLimp_GetProcAddresses( void ) {
 		sscanf( version, "%d.%d", &qglMajorVersion, &qglMinorVersion );
 	}
 
-	if ( QGL_VERSION_ATLEAST( 1, 1 ) ) {
+	if ( QGL_VERSION_ATLEAST( 1, 2 ) ) {
 		QGL_1_1_PROCS;
 		QGL_DESKTOP_1_1_PROCS;
 	} else if ( qglesMajorVersion == 1 && qglesMinorVersion >= 1 ) {
