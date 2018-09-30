93dd14c9fb7784bba73cb6f175f883ecf62101c9
Zack Middleton <zack@cloemail.com>
2018-09-30 14:49:12 -0500

    Disable pulseaudio capture regardless of SDL version
    SDL changed version to 2.0.9 but the pulseaudio capture bug isn't fixed
    yet. Remove my version check.

--- code/sdl/sdl_snd.c
+++ code/sdl/sdl_snd.c
@@ -188,9 +188,6 @@ qboolean SNDDMA_Init(void)
 	SDL_AudioSpec desired;
 	SDL_AudioSpec obtained;
 	int tmp;
-#ifdef USE_SDL_AUDIO_CAPTURE
-	SDL_version sdlVersion;
-#endif
 
 	if (snd_inited)
 		return qtrue;
@@ -283,11 +280,10 @@ qboolean SNDDMA_Init(void)
 #ifdef USE_SDL_AUDIO_CAPTURE
 	// !!! FIXME: some of these SDL_OpenAudioDevice() values should be cvars.
 	s_sdlCapture = Cvar_Get( "s_sdlCapture", "1", CVAR_ARCHIVE | CVAR_LATCH );
-	// !!! FIXME: hopefully pulseaudio capture will be fixed in SDL 2.0.9... https://bugzilla.libsdl.org/show_bug.cgi?id=4087
-	SDL_GetVersion(&sdlVersion);
-	if (sdlVersion.major == 2 && sdlVersion.minor == 0 && sdlVersion.patch < 9 && Q_stricmp(SDL_GetCurrentAudioDriver(), "pulseaudio") == 0)
+	// !!! FIXME: pulseaudio capture records audio the entire time the program is running. https://bugzilla.libsdl.org/show_bug.cgi?id=4087
+	if (Q_stricmp(SDL_GetCurrentAudioDriver(), "pulseaudio") == 0)
 	{
-		Com_Printf("SDL audio capture support disabled (pulseaudio capture does not work correctly with SDL %d.%d.%d)\n", sdlVersion.major, sdlVersion.minor, sdlVersion.patch);
+		Com_Printf("SDL audio capture support disabled for pulseaudio (https://bugzilla.libsdl.org/show_bug.cgi?id=4087)\n");
 	}
 	else if (!s_sdlCapture->integer)
 	{
