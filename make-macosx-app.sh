b1ffdd1be09d26dca5d918d2f22fafb3f8058f42
Tom Kidd <tomkidd@gmail.com>
2021-07-10 19:58:06 -0500

    Removed signature from SDL dylib, enabled dark mode on macOS.

--- make-macosx-app.sh
+++ make-macosx-app.sh
@@ -372,6 +372,8 @@ PLIST="${PLIST}
     <string>NSApplication</string>
     <key>NSHighResolutionCapable</key>
     <false/>
+	<key>NSRequiresAquaSystemAppearance</key>
+	<false/>
 </dict>
 </plist>
 "
